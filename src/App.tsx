763cdbb2a69df97513a740447dd355b00ce9b62b
chetanv18~ <chetan@gully2global.com>
2025-04-06 17:19:16 +0530

    new

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ const App = () => {
   return (
     <main className="min-h-screen bg-black dark:bg-gray-700 text-white sm:py-12">
       <div className="max-w-3xl mx-auto p-4">
-        <h1 className="text-3xl font-bold text-center mb-8 text-blue-400">Todo React + Typescript</h1>
+        <h1 className="text-3xl font-bold text-center mb-80 lg:mb-12 text-white dark:text-400 lg:text-4xl">Todo React + Typescript</h1>
         <div className="bg-gray-900 rounded-lg p-6 shadow-lg border border-gray-800">
           <NavBar/>
           <AddTodo/>
